dfeec53a0b40e7240437e8ff1c66448d2ca33444
Marek Orłowski <morlowski@ittouch.pl>
2018-07-27 14:20:33 +0200

    angular

--- src/main/java/contollers/TestContoller.java
+++ src/main/java/contollers/TestContoller.java
@@ -1,20 +1,16 @@
-import org.springframework.boot.SpringApplication;
+package contollers;
+
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@EnableAutoConfiguration
-public class Main {
+public class TestContoller {
+
     @RequestMapping("/")
     @ResponseBody
     public String mainPage(){
         return "To działa";
-    }
-    public static void main(String[] args){
-        SpringApplication.run(Main.class, args);
     }
 }
